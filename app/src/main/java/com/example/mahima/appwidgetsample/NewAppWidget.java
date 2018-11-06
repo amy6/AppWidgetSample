5ea4ea606938e8963382f85c4dc728e3daf26ed6
Mahima <mahima.6.ms@gmail.com>
2018-11-06 11:22:51 +0530

    display update count and last update time for widgets

--- app/src/main/java/com/example/mahima/appwidgetsample/NewAppWidget.java
+++ app/src/main/java/com/example/mahima/appwidgetsample/NewAppWidget.java
@@ -3,19 +3,38 @@ package com.example.mahima.appwidgetsample;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
+    public static final String sharedPrefFile = "com.example.mahima.appwidgetsample";
+    public static final String COUNT_KEY = "count";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+
+        SharedPreferences sharedPref = context.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE);
+        int count = sharedPref.getInt(COUNT_KEY + appWidgetId , 0);
+        count++;
+
+        String dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+        views.setTextViewText(R.id.appwidget_update,
+                context.getResources().getString(R.string.date_count_format, count, dateString));
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(COUNT_KEY + appWidgetId, count);
+        editor.apply();
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
